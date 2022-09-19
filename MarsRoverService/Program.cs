62636fb48be83e55ee72bf2c39fcb9e901ab2189
Ija Saporenkova <ija.sha@gmail.com>
2022-09-19 17:26:54 +0100

    Add readme

--- MarsRoverService/Program.cs
+++ MarsRoverService/Program.cs
@@ -8,29 +8,16 @@
             var size = input.Split().Select(x=>Convert.ToInt32(x)).ToArray();
             var plateau = new Plateau(size[0], size[1]);
             var mc = new MissionControl(plateau);
-            //var r = new Rover("0 0 N");
-            //r.PrintCurrentPosition();
-            //Console.WriteLine(r.Direction);
-            //if (r.Direction == 'N') r.Direction = 'W';
-            //r.SpinLeft();
-            //r.Direction = 'Q';
-            //r.PrintCurrentPosition();
-            //mc.AddRover(new Rover("0 0 N"));
-            //mc.PrintAllRoverPositions();
-
-            //MissionControl mc = new MissionControl(new Plateau(5, 5));
-            var rover1 = new Rover("1 2 N");
-            //var rover2 = new Rover("3 4 S");
-            mc.AddRover(rover1);
-            //mc.AddRover(rover2);
-            //rover1.PrintCurrentPosition();
-            //rover2.PrintCurrentPosition();
-            mc.MakeRoverToMakeJourney(rover1, "LMLMLMLMM");
+            var roverCount = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i < roverCount; i++)
+            {
+                var firstPosition = Console.ReadLine();
+                var rover = new Rover(firstPosition);
+                mc.AddRover(rover);
+                var instructions = Console.ReadLine();
+                mc.MakeRoverToMakeJourney(rover, instructions);
+            }
             mc.PrintAllRoverPositions();
-            //mc.MoveRoverForward(rover1);
-            //mc.PrintAllRoverPositions();
-
         }
-
     }
 }
