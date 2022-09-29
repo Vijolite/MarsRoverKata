b97cc461966b884bc9f844548e79c384fd9567ac
Ija Saporenkova <ija.sha@gmail.com>
2022-09-29 08:36:20 +0100

    Separated RoverTests and MissionControlTests into fidderent files

--- MarsRoverService.Tests/MissionControlTests.cs
+++ MarsRoverService.Tests/MissionControlTests.cs
@@ -7,36 +7,18 @@ namespace MarsRoverService.Tests
 {
     [TestClass]
 
-    public class Tests
+    public class MissionControlTests
     {
+        private Plateau plateau;
+        private MissionControl mc;
+
         [SetUp]
         public void Setup()
         {
-            Plateau plateau = new Plateau(5, 5);
-            MissionControl missionControl = new MissionControl(plateau);
+            plateau = new Plateau(5, 5);
+            mc = new MissionControl(plateau);
         }
-      
 
-        [TestCase("4 4 W", "4 4 S")]
-        [TestCase("3 3 S", "3 3 E")]
-        [TestCase("3 4 E", "3 4 N")]
-        [TestCase("0 0 N", "0 0 W")]
-        public void TestRoverSpinLeft(string initial, string result)
-        {
-            var rover = new Rover(initial);
-            rover.SpinLeft();
-            rover.GetCurrentPosition().Should().Be(result);
-        }
-        [TestCase("4 4 W", "4 4 N")]
-        [TestCase("3 3 S", "3 3 W")]
-        [TestCase("3 4 E", "3 4 S")]
-        [TestCase("0 0 N", "0 0 E")]
-        public void TestRoverSpinRight(string initial, string result)
-        {
-            var rover = new Rover(initial);
-            rover.SpinRight();
-            rover.GetCurrentPosition().Should().Be(result);
-        }
         [TestMethod]
         public void TestAddTwoRoversToTheSameSquare()
         {
---
+++ MarsRoverService.Tests/RoverTests.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace MarsRoverService.Tests
+{
+    [TestClass]
+
+    public class RoverTests
+    {
+
+        [TestCase("4 4 W", "4 4 S")]
+        [TestCase("3 3 S", "3 3 E")]
+        [TestCase("3 4 E", "3 4 N")]
+        [TestCase("0 0 N", "0 0 W")]
+        public void TestRoverSpinLeft(string initial, string result)
+        {
+            var rover = new Rover(initial);
+            rover.SpinLeft();
+            rover.GetCurrentPosition().Should().Be(result);
+        }
+        [TestCase("4 4 W", "4 4 N")]
+        [TestCase("3 3 S", "3 3 W")]
+        [TestCase("3 4 E", "3 4 S")]
+        [TestCase("0 0 N", "0 0 E")]
+        public void TestRoverSpinRight(string initial, string result)
+        {
+            var rover = new Rover(initial);
+            rover.SpinRight();
+            rover.GetCurrentPosition().Should().Be(result);
+        }
+
+    }
+}
